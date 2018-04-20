e2996deed45b50371b7ac51164065aabb8eb8d63
Josue Arce <josuerc026@gmail.com>
2018-04-19 23:01:48 -0400

    initial commit

---
+++ dist/js/app.js
@@ -0,0 +1,62 @@
+var tttBoxes = document.querySelectorAll('.ttt-container div'),
+    turnContainer = document.querySelector('.player-turns'),
+    pTurns = 1,
+    p1Score = [],
+    p2Score = [];
+
+var successfulCombos = {
+    row1: [1, 2, 3],
+    row2: [4, 5, 6],
+    row3: [7, 8, 9],
+    col1: [1, 4, 7],
+    col2: [2, 5, 8],
+    col3: [3, 6, 9],
+    diagonal1: [3, 5, 7],
+    diagonal2: [1, 5, 9],
+};
+
+
+var checkScore = function(scoreArr, player) {
+    var ordered = scoreArr.sort();
+    for (var key in successfulCombos) {
+        if (!successfulCombos.hasOwnProperty(key)) continue;
+        if (successfulCombos[key].join() === ordered.join()) {
+            void 0;
+        }
+    }
+};
+
+var playerOne = function(pickedBox, boxNumber) {
+    p1Score.push(boxNumber);
+
+    pickedBox.classList.add('p1-active');
+    pickedBox.setAttribute('data-taken', 'true');
+
+    turnContainer.innerText = 'Player 2 Turn';
+
+    if (p1Score.length === 3) {
+        checkScore(p1Score, 'Player 1');
+    }
+};
+
+var playerTwo = function(pickedBox, boxNumber) {
+
+    p2Score.push(boxNumber);
+
+    pickedBox.classList.add('p2-active');
+    pickedBox.setAttribute('data-taken', 'true');
+
+    turnContainer.innerText = 'Player 1 Turn';
+
+    if (p2Score.length === 3) {
+        checkScore(p2Score, 'Player 2');
+    }
+};
+tttBoxes.forEach(function(item, index) {
+    item.addEventListener('click', function() {
+        var box = this;
+        var i = index + 1;
+        pTurns % 2 === 0 ? playerTwo(box, i) : playerOne(box, i);
+        pTurns++;
+    });
+});
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    pretty = require('gulp-jsbeautifier'),
+    stripDebug = require('gulp-strip-debug');
+
+gulp.task('sass', function(){
+  gulp.src('src/css/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('styles',function(){
+  gulp.src('dist/css/*.css')
+      .pipe(pretty())
+      .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('scripts', function(){
+  gulp.src('src/js/*.js')
+      .pipe(stripDebug())
+      .pipe(pretty())
+      .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('default', ['sass', 'watch', 'scripts', 'styles']);
+gulp.task('watch', function(){
+  gulp.watch('src/css/*.scss', ['sass']);
+  gulp.watch('src/js/*.js', ['scripts']);
+  gulp.watch('dist/css/*.css', ['styles']);
+});
